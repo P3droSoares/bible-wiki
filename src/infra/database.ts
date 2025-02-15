dc28f6e0c4d2f98b571751ff005cb33e92dbbcc0
P3droSoares <pedro.soares.contato@hotmail.com>
2025-02-15 15:35:54 -0300

    adiciona o `compose.yaml` e o modulo `database.ts`

---
+++ src/infra/database.ts
@@ -0,0 +1,21 @@
+import { Client, QueryConfig } from 'pg'
+
+async function query(queryObjects: QueryConfig) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
+  });
+
+  await client.connect()
+  const response = await client.query(queryObjects)
+  await client.end()
+
+  return response
+}
+
+export default {
+  query
+}
---
+++ src/pages/api/v1/status/index.ts
@@ -0,0 +1,21 @@
+import database from "@/infra/database";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string
+}
+
+async function status(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const response = await database.query({
+    text: "SELECT 1 + 1;"
+  })
+  
+  console.log(response.rows)
+
+  return res.status(200).json({message: "Tudo certo!"});
+}
+
+export default status
