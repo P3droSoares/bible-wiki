b94f9201eaf045bdd59de8c0d03dcf05dc073f54
P3droSoares <pedro.soares.contato@hotmail.com>
2025-02-16 22:12:42 -0300

    modifica a  `home`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
 function Home(){
-  return <h1>BibleWiki</h1>
+  return (
+    <>
+      <div style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '0.8rem'
+      }}>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '0.8rem'
+        }}>
+          <img width={32} src="https://images.emojiterra.com/google/android-10/512px/1f4d6.png"/>
+          <h1 style={{
+            margin: 0,
+            fontSize: "1.4rem",
+          }}>BibleWiki</h1>
+        </div>
+        <p style={{
+          margin: 0,
+          fontSize: "1.3rem",
+        }}>Tem algo bom vindo ai...</p>
+      </div>
+    </>
+  )
 }
 
 export default Home
