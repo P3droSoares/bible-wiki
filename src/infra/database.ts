89efbded29738194bd805a9b246bd74b7314785d
P3droSoares <pedro.soares.contato@hotmail.com>
2025-03-16 09:07:46 -0300

    base

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,6 +1,6 @@
 import { Client, QueryConfig } from "pg";
 
-async function query(queryString: string) {
+async function query(queryString: QueryConfig | string) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: Number(process.env.POSTGRES_PORT),
@@ -10,10 +10,15 @@ async function query(queryString: string) {
   });
 
   await client.connect();
-  const response = await client.query(queryString);
-  await client.end();
 
-  return response;
+  try {
+    const response = await client.query(queryString);
+    return response;
+  } catch (error) {
+    throw new Error("Erro ao executar query");
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
